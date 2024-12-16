2e26a863cb7d936fb5a8a60bc5b9ab2a32483f06
Zentry96 <45236139+Zentry96@users.noreply.github.com>
2024-12-16 11:34:39 +0100

    Test commit

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace DogAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Test"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
