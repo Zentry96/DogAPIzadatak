7e6a2974428614301d88888ee81b36cf302445d4
Zentry96 <45236139+Zentry96@users.noreply.github.com>
2024-12-16 12:14:39 +0100

    Deleting dummy files, Creating controller and trying to fetch from the API

---
+++ Controllers/DogAPIController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DogAPIController : ControllerBase
+    {
+        [HttpGet("")]
+        public async Task<IActionResult> GetList()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://dog.ceo/");
+
+            var response = await client.GetAsync("api/breed/terrier/list");
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStreamAsync();
+                return Ok(result);
+            }
+
+            return BadRequest();
+        }
+    }
+}
